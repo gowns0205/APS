a2c9d02eb00faf6f573212da0319ae44d80e3c1b
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-03-05 15:57:34 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 296 ms, Memory: 19504 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int cnt = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			String s = br.readLine();
+			System.out.println(isPailndrome(s) + " " + cnt);
+			cnt = 0;
+		}
+	}
+
+	static int isPailndrome(String s) {
+		return recursion(s, 0, s.length() - 1);
+	}
+
+	static int recursion(String s, int l, int r) {
+		cnt++;
+		if (l >= r)
+			return 1;
+		else if (s.charAt(l) != s.charAt(r))
+			return 0;
+		else
+			return recursion(s, l + 1, r - 1);
+	}
+}
