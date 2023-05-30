45fdb45a60d2ba7f09e272bf0e9769666042325b
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-05-30 20:12:44 +0900

    [Silver V] Title: 단어 정렬, Time: 364 ms, Memory: 27184 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		String[] arr = new String[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			if(i==0)
+				sb.append(arr[i]).append("\n");
+			else if(!arr[i].equals(arr[i-1]))
+				sb.append(arr[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
