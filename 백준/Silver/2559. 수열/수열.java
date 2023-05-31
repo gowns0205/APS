4cc156641f99eaf2ec5c81ab99d4f439da4fd264
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-05-31 12:58:32 +0900

    [Silver III] Title: 수열, Time: 260 ms, Memory: 23448 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,29 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Queue<Integer> queue = new LinkedList<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		int tmpSum = 0;
-		int maxSum = Integer.MIN_VALUE;
-		for (int i = 1; i <= N; i++) {
-			int tmp = Integer.parseInt(st2.nextToken());
-			tmpSum += tmp;
-			queue.add(tmp);
-			if (i >= K) {
-				if (tmpSum > maxSum)
-					maxSum = tmpSum;
-				tmpSum -= queue.poll();
-			}
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		long maxSum = 0;
+		long tmpSum = 0;
+		for (int i = 0; i < K; i++) {
+			tmpSum += arr[i];
+		}
+		maxSum = tmpSum;
+		int idx = 0;
+		while (true) {
+			if (idx + K > N - 1)
+				break;
+			tmpSum -= arr[idx];
+			tmpSum += arr[idx + K];
+			if (tmpSum > maxSum)
+				maxSum = tmpSum;
+			idx++;
 		}
 		System.out.println(maxSum);
 	}
