13c9fb90e82f32727ba43fee0393bfce2f9d300e
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-06-16 15:17:16 +0900

    Update 팩토리얼 0의 개수.java

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 접근 방식
+// 1. 어떤 숫자에서 뒤에 붙는 0의 개수는 10의 계수와 같다. 
+// 2. 10은 2와 5의 곱이다. 
+// 3. 연속된 수를 계속 곱하면 소인수분해 했을때 5보다는 2 개수가 많게 된다.  
+// 4. 그러므로 [뒤에 붙는 0의 개수 = 소인수 5의 개수] 이다.
+
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
