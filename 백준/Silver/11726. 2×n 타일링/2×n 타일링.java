2e02a2af96cdac3434ecd15e8e963f637ec9a570
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-07-10 16:05:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		dp = new int[n];
+		fillTile(n - 1);
+		System.out.println(dp[n - 1]);
+		br.close();
+	}
+
+	public static int fillTile(int cur) {
+		// Base
+		if (cur == 0) {
+			dp[0] = 1;
+			return 1;
+		} else if (cur == 1) {
+			dp[1] = 2;
+			return 2;
+		}
+
+		if (dp[cur] != 0) {
+			return dp[cur];
+		}
+		dp[cur] = (fillTile(cur - 1) + fillTile(cur - 2))%10007;
+		return dp[cur];
+	}
+}
