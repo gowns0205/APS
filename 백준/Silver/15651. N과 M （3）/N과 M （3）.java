8e7b81a4d582c3c622942b11329c6d2a376915e5
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-09-17 16:28:49 +0900

    Update N과 M （3）.java

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-// 중복조합 문제
+// 중복순열 문제
 // StringBuilder 안 쓰면 시간초과 발생
 public class Main {
 
@@ -14,12 +14,12 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int[] out = new int[M];
-		combination(out, M, 0);
+		permutation(out, M, 0);
 		System.out.println(sb);
 		br.close();
 	}
 
-	static void combination(int[] out, int m, int depth) {
+	static void permutation(int[] out, int m, int depth) {
 		if (depth == m) {
 			for (int i = 0; i < m; i++)
 				sb.append(out[i] + " ");
@@ -28,7 +28,7 @@ public class Main {
 		}
 		for (int i = 1; i <= N; i++) {
 			out[depth] = i;
-			combination(out, m, depth + 1);
+			permutation(out, m, depth + 1);
 		}
 	}
 }
