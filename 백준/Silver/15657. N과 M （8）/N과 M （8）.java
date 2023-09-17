eba69abe6d56e65a1bf6495a9e392245c1bc23e0
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-09-17 18:52:21 +0900

    [Silver III] Title: N과 M (8), Time: 196 ms, Memory: 23528 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+// 중복조합 문제 
+public class Main {
+
+	static int N;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] num = new int[N + 1];
+		boolean[] visited = new boolean[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		int[] out = new int[M];
+		combination(num, visited, M, 0, out, 1);
+		System.out.println(sb);
+		br.close();
+	}
+
+	static void combination(int[] num, boolean[] visited, int r, int depth, int[] out, int start) {
+		if (depth == r) {
+			for (int i = 0; i < r; i++) {
+				sb.append(out[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+			out[depth] = num[i];
+			combination(num, visited, r, depth + 1, out, i);
+		}
+	}
+}
