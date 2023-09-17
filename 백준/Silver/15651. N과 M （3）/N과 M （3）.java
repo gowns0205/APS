eecf00238c977a678428aa1ceb473f2d42fb45e8
Haejune Lee <122420097+haegod@users.noreply.github.com>
2023-09-17 14:00:51 +0900

    [Silver III] Title: N과 M (3), Time: 816 ms, Memory: 308904 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+// 중복조합 문제
+// StringBuilder 안 쓰면 시간초과 발생
+public class Main {
+
+	static int N;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] out = new int[M];
+		combination(out, M, 0);
+		System.out.println(sb);
+		br.close();
+	}
+
+	static void combination(int[] out, int m, int depth) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++)
+				sb.append(out[i] + " ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			out[depth] = i;
+			combination(out, m, depth + 1);
+		}
+	}
+}
