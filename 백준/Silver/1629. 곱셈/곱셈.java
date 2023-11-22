5a39e5e5616d4c8ebdb18317b5b346a6451b37f5
Haejune Lee <122420097+gowns0205@users.noreply.github.com>
2023-11-22 14:40:40 +0900

    [Silver I] Title: 곱셈, Time: 220 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static long C;
+
+	static long pow(long A, long exponent) {
+		if (exponent == 1)
+			return A % C;
+		long temp = pow(A, exponent / 2) ;
+		if (exponent % 2 == 1)
+			return (temp * temp % C) * A % C;
+		return temp * temp % C;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		C = sc.nextLong();
+		System.out.println(pow(A, B));
+	}
+
+}
